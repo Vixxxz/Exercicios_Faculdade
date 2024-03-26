e6b5e6338f906487df4eb370f739d5edb77ee3ca
Ryan Takata de Siqueira <ryantakata@outlook.com>
2024-03-25 21:08:03 -0300

    empilha string

---
+++ estrutura_de_dados/lista1/pilhas/pilhaString.c
@@ -0,0 +1,19 @@
+/*Exercício 5. Pilha de strings [versão 1]
+Qual será a saída, se o usuário digite as cadeias "um", "dois" e "tres"? Por quê?*/
+
+#include <stdio.h>
+#include "pilha.h" 
+
+int main(void) 
+{
+    Pilha P = pilha(5);
+    char s[11];
+    for(int i=1; i<=3; i++) 
+    {
+        printf("? ");
+        gets(s);
+        empilha(strdup(s),P);
+    }
+    while( !vaziap(P) ) puts(desempilha(P));
+    return 0;
+}
